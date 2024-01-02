be7a790bef75fbb5cf99ca6be06709401ea6afdb
GauravChandel <gaurav7217444@gmail.com>
2024-01-02 15:48:16 +0530

    changed the asserstion

--- cypress/e2e/AmazonLoginUser.cy.js
+++ cypress/e2e/AmazonLoginUser.cy.js
@@ -4,7 +4,7 @@ import {LogIn}  from '../Pages/Login'
 const loginuser = new LogIn()
 import cred from '../fixtures/LoginCred.json'
 
-describe('Check the flow of add to cart', () => {
+describe('User Log in with its credentials', () => {
   it('Login the user', () => {
       loginuser.OpenURL()
       loginuser.clickonsignin()
@@ -14,7 +14,7 @@ describe('Check the flow of add to cart', () => {
       loginuser.Clickcheckbox()
       loginuser.Clickcheckbox()
       loginuser.SignInclick()
-      cy.url().should('eq', 'https://www.amazon.in/?ref_=nav_ya_signin')
+      cy.url().should('include', 'https://www.amazon.in/')
       // cy.get('#nav-logo-sprites').wait(1000).should("have.attr", "href").and("not.be.empty")
      
   })
