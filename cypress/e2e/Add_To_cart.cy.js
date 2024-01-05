0c489145c7bccdb4ca530cda7207ae2627a0de09
GauravChandel <gaurav7217444@gmail.com>
2024-01-05 13:49:22 +0530

    Done

--- cypress/e2e/Add_To_cart.cy.js
+++ cypress/e2e/Add_To_cart.cy.js
@@ -16,7 +16,7 @@ describe('Add to cart any item',()=>{
         obj1.typeinsearchfield()
         cy.contains('Results').should('be.visible')
         obj1.clickonMobile()
-        cy.get('.a-price-whole').should('have.text', '10,999.10,999.10,999.10,999.10,99910,999.8,999.9,999.')
+        cy.get('.a-price-whole').should('have.text', '10,999.10,999.10,999.10,999.10,99910,999.8,999.')
         obj1.clickonaddtocartbutton()
         cy.get('div>h4.a-alert-heading').eq(5).contains('Added to Cart')
     })
